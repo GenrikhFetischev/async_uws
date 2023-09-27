335a3547469d9a3fe78da3c3607ad96f0add3bf2
Genrikh Fetishchev <genrikhfetischev@gmail.com>
2023-09-27 20:59:26 +0200

    reexported ListenSocket

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod ws_behavior;
 pub mod ws_message;
 
 pub mod uwebsockets_rs {
+    pub use uwebsockets_rs::listen_socket::ListenSocket;
     pub use uwebsockets_rs::us_socket_context_options::UsSocketContextOptions;
     pub use uwebsockets_rs::websocket::Opcode;
     pub use uwebsockets_rs::websocket_behavior::CompressOptions;
